753951bf7a36c6bb9f8b8a8e39ddae4c8507ea0e
Luis2387 <l.montesacuna@student.fdu.edu>
2024-11-27 04:37:10 -0800

    create superuser

--- jobs/views.py
+++ jobs/views.py
@@ -14,6 +14,8 @@ from rest_framework import viewsets, mixins
 from django.contrib.auth import logout
 from rest_framework_simplejwt.tokens import RefreshToken
 from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth.models import User
+
 
 
 class RegisterView(APIView):
